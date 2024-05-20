9c021151cb48d09e125033c686d5b7bbf01c7706
Rohit <rautrohit.rr123@gmail.com>
2024-05-20 20:41:41 +0545

    Add User Management

--- src/utils/users.js
+++ src/utils/users.js
@@ -32,9 +32,27 @@ const addUser = ({id, username, room}) => {
     return {user}
 }
 
-addUser({
-    id: 20,
-    username: 'rohit',
-    room: 'new'
-})
-console.log(users)
+const removeUser = (id) =>{
+    const index = users.findIndex((user)=> user.id === id)
+
+    if(index !== -1){
+        return users.splice(index, 1)
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user)=>user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter((user)=>user.room === room)
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
+
+
