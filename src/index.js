0038387fe7eede6f9fa47a608513c945fd4f07e6
Rohit <rautrohit.rr123@gmail.com>
2024-05-21 13:39:21 +0545

    Feat: Add user traking Tarking users joining and leaving

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const socketio = require("socket.io");
 
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const {addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users')
 
 const app = express();
 const server = http.createServer(app);
@@ -18,29 +19,43 @@ io.on("connection", (socket) => {
   console.log("New websocket connection");
 
 
-  socket.on('join', ({username, room})=>{
-    socket.join(room)
+  socket.on('join', ({username, room}, callback)=>{
+    const {error, user} = addUser({id:socket.id, username, room})
+
+    if(error){
+      return callback(error)
+    }
+
+    socket.join(user.room)
 
     socket.emit("message", generateMessage('Welcome!'));
-    socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined!`));
+    socket.broadcast.to(user.room).emit("message", generateMessage(user.username,`${user.username} has joined!`));
+
+    callback();
      
   })
 
   socket.on("sendMessage", (message, callback) => {
-    io.to('new').emit("message", generateMessage(message));
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", generateMessage(message));
     callback();
   });
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id);
+    io.to(user.room).emit(
       "locationMessage",
-      generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+      generateLocationMessage(user.username,`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     );
     callback("Location shared!!");
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user disconnected!"));
+    const user = removeUser(socket.id);
+
+    if(user){
+      io.to(user.room).emit("message", generateMessage(`${user.username} has disconnected!`));
+    }
   });
 });
 
